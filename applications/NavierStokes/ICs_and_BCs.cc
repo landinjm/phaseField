07855861c11b0397c286b2d27c2d9c883350cb89
landinjm <nephrite@DESKTOP-DQBN823>
2024-01-24 08:23:52 -0500

    Added Navier Stokes code

---
+++ applications/NavierStokes/ICs_and_BCs.cc
@@ -0,0 +1,72 @@
+// ===========================================================================
+// FUNCTION FOR INITIAL CONDITIONS
+// ===========================================================================
+
+template <int dim, int degree>
+void customPDE<dim,degree>::setInitialCondition(const dealii::Point<dim> &p, const unsigned int index, double & scalar_IC, dealii::Vector<double> & vector_IC){
+    // ---------------------------------------------------------------------
+    // ENTER THE INITIAL CONDITIONS HERE
+    // ---------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the initial condition for each variable
+    // according to its variable index
+
+    // The initial condition is a set of overlapping circles/spheres defined
+    // by a hyperbolic tangent function. The center of each circle/sphere is
+    // given by "center" and its radius is given by "radius".
+
+    //Velocity
+    if(index == 0){
+        for(unsigned int d=0; d<dim; d++){ 
+            vector_IC(d) = 0.0;
+        }
+        
+        /*double center[3] = {0.0, 0.5, 0.5};
+        double dist = 0.0;
+        for (unsigned int dir = 0; dir < dim; dir++){
+            dist += (p[dir]-center[dir]*userInputs.domain_size[dir])*(p[dir]-center[dir]*userInputs.domain_size[dir]);
+        }
+        dist = std::sqrt(dist);
+        vector_IC(0) = 0.5*(1.0-tanh((dist-0.2)/0.1));*/
+    }
+    //Pressure
+    if (index == 1){
+        scalar_IC = 0.0;
+    }
+    //Misc
+    else{
+        scalar_IC = 0.0;
+    }
+   
+    // ---------------------------------------------------------------------
+}
+
+// ===========================================================================
+// FUNCTION FOR NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS
+// ===========================================================================
+
+template <int dim, int degree>
+void customPDE<dim,degree>::setNonUniformDirichletBCs(const dealii::Point<dim> &p, const unsigned int index, const unsigned int direction, const double time, double & scalar_BC, dealii::Vector<double> & vector_BC)
+{
+    // --------------------------------------------------------------------------
+    // ENTER THE NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS HERE
+    // --------------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the boundary condition for each variable
+    // according to its variable index. This function can be left blank if there
+    // are no non-uniform Dirichlet boundary conditions. For BCs that change in
+    // time, you can access the current time through the variable "time". The
+    // boundary index can be accessed via the variable "direction", which starts
+    // at zero and uses the same order as the BC specification in parameters.in
+    // (i.e. left = 0, right = 1, bottom = 2, top = 3, front = 4, back = 5).
+
+    if(index == 0){
+        if(direction == 0){
+            double normalizedPos = p[1]/userInputs.domain_size[1];
+            vector_BC(0) = 4.0*normalizedPos*(1.0-normalizedPos);
+        }
+    }
+
+    // -------------------------------------------------------------------------
+
+}
---
+++ applications/NavierStokes/equations.cc
@@ -0,0 +1,167 @@
+// =================================================================================
+// Set the attributes of the primary field variables
+// =================================================================================
+// This function sets attributes for each variable/equation in the app. The
+// attributes are set via standardized function calls. The first parameter for each
+// function call is the variable index (starting at zero). The first set of
+// variable/equation attributes are the variable name (any string), the variable
+// type (SCALAR/VECTOR), and the equation type (EXPLICIT_TIME_DEPENDENT/
+// TIME_INDEPENDENT/AUXILIARY). The next set of attributes describe the
+// dependencies for the governing equation on the values and derivatives of the
+// other variables for the value term and gradient term of the RHS and the LHS.
+// The final pair of attributes determine whether a variable represents a field
+// that can nucleate and whether the value of the field is needed for nucleation
+// rate calculations.
+
+void variableAttributeLoader::loadVariableAttributes(){
+	// Variable 0 - the velocity vector
+	set_variable_name				(0,"u");
+	set_variable_type				(0,VECTOR);
+	set_variable_equation_type		(0,EXPLICIT_TIME_DEPENDENT);
+
+    set_dependencies_value_term_RHS(0, "u,grad(u),grad(P)");
+    set_dependencies_gradient_term_RHS(0, "grad(u)");
+
+	// Variable 1 - pressure
+	set_variable_name				(1,"P");
+	set_variable_type				(1,SCALAR);
+	set_variable_equation_type		(1,TIME_INDEPENDENT);
+
+    set_dependencies_value_term_RHS(1, "grad(u)");
+    set_dependencies_gradient_term_RHS(1, "grad(P),u");
+	set_dependencies_value_term_LHS(1, "");
+    set_dependencies_gradient_term_LHS(1, "grad(change(P))");
+
+}
+
+// =============================================================================================
+// explicitEquationRHS (needed only if one or more equation is explict time dependent)
+// =============================================================================================
+// This function calculates the right-hand-side of the explicit time-dependent
+// equations for each variable. It takes "variable_list" as an input, which is a list
+// of the value and derivatives of each of the variables at a specific quadrature
+// point. The (x,y,z) location of that quadrature point is given by "q_point_loc".
+// The function outputs two terms to variable_list -- one proportional to the test
+// function and one proportional to the gradient of the test function. The index for
+// each variable in this list corresponds to the index given at the top of this file.
+
+template <int dim, int degree>
+void customPDE<dim,degree>::explicitEquationRHS(variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
+				 dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const {
+
+	//Step one of the Chorin projection
+	if(!ChorinSwitch){
+		//Grab derivative model variable
+		vectorvalueType u = variable_list.get_vector_value(0);
+		vectorgradType ux = variable_list.get_vector_gradient(0);
+	
+		//Calculating the advection term
+		vectorvalueType advecTerm;
+		for(unsigned int i=0; i<dim; i++){
+			for(unsigned int j=0; j<dim; j++){
+				advecTerm[i] += u[j]*ux[i][j];
+			}
+		}
+
+		//Setting the expressions for the terms in the governing equations
+		vectorvalueType eq_u = u-constV(userInputs.dtValue)*advecTerm;
+		vectorgradType eqx_u = constV(-nu*userInputs.dtValue)*ux;
+
+		if(this->currentIncrement <= switchToFractional){
+			scalargradType Px = variable_list.get_scalar_gradient(1);
+			eq_u -= constV(userInputs.dtValue/rho)*Px;
+		}
+		//Submitting the terms for the governing equations
+		variable_list.set_vector_value_term_RHS(0,eq_u);
+		variable_list.set_vector_gradient_term_RHS(0,eqx_u);
+	}
+
+	//Step three of the Chorin projection
+	if(ChorinSwitch == true){
+		//Grab derivative model variable
+		vectorvalueType u = variable_list.get_vector_value(0);
+		scalargradType Px = variable_list.get_scalar_gradient(1);
+	
+		//Setting the expressions for the terms in the governing equations
+		vectorvalueType eq_u = u-constV(userInputs.dtValue/rho)*Px;
+
+		if(this->currentIncrement <= switchToFractional){
+			eq_u = u;
+		}
+
+		//This dummy variable is neccessary to remove the residual from step 1 chorin
+		vectorgradType dummy;
+
+		//Submitting the terms for the governing equations
+		variable_list.set_vector_value_term_RHS(0,eq_u);
+		variable_list.set_vector_gradient_term_RHS(0,0.0*dummy);
+	}
+
+}
+
+// =============================================================================================
+// nonExplicitEquationRHS (needed only if one or more equation is time independent or auxiliary)
+// =============================================================================================
+// This function calculates the right-hand-side of all of the equations that are not
+// explicit time-dependent equations. It takes "variable_list" as an input, which is
+// a list of the value and derivatives of each of the variables at a specific
+// quadrature point. The (x,y,z) location of that quadrature point is given by
+// "q_point_loc". The function outputs two terms to variable_list -- one proportional
+// to the test function and one proportional to the gradient of the test function. The
+// index for each variable in this list corresponds to the index given at the top of
+// this file.
+
+template <int dim, int degree>
+void customPDE<dim,degree>::nonExplicitEquationRHS(variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
+				 dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const {
+	
+	if(this->currentIncrement <= switchToFractional){
+		vectorgradType ux = variable_list.get_vector_gradient(0);
+		scalargradType Px = variable_list.get_scalar_gradient(1);
+
+		scalarvalueType eq_P;
+		for(unsigned int i = 0; i<dim; i++){
+			for(unsigned int j = 0; j<dim; j++){
+				eq_P += ux[i][j]*ux[j][i];
+			}
+		}
+
+		variable_list.set_scalar_value_term_RHS(1,eq_P);
+		variable_list.set_scalar_gradient_term_RHS(1,-Px);
+	}
+	else{
+		vectorvalueType u = variable_list.get_vector_value(0);
+		scalargradType Px = variable_list.get_scalar_gradient(1);
+
+		vectorvalueType eq_P = constV(rho/userInputs.dtValue)*u;
+
+		variable_list.set_scalar_gradient_term_RHS(1,eq_P-Px);
+	}
+
+}
+
+// =============================================================================================
+// equationLHS (needed only if at least one equation is time independent)
+// =============================================================================================
+// This function calculates the left-hand-side of time-independent equations. It
+// takes "variable_list" as an input, which is a list of the value and derivatives of
+// each of the variables at a specific quadrature point. The (x,y,z) location of that
+// quadrature point is given by "q_point_loc". The function outputs two terms to
+// variable_list -- one proportional to the test function and one proportional to the
+// gradient of the test function -- for the left-hand-side of the equation. The index
+// for each variable in this list corresponds to the index given at the top of this
+// file. If there are multiple elliptic equations, conditional statements should be
+// sed to ensure that the correct residual is being submitted. The index of the field
+// being solved can be accessed by "this->currentFieldIndex".
+
+template <int dim, int degree>
+void customPDE<dim,degree>::equationLHS(variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
+		dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const {
+	
+	// --- Getting the values and derivatives of the model variables ---
+  	scalargradType DPx = variable_list.get_change_in_scalar_gradient(1);
+
+	// --- Submitting the terms for the governing equations ---
+	variable_list.set_scalar_gradient_term_LHS(1,DPx);
+
+}
