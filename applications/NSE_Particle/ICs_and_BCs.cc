d6a687e643c60dbd27e77de0ee7ce5c31257b44d
landinjm <nephrite@DESKTOP-DQBN823>
2024-07-02 12:09:45 -0400

    Adding application (WIP) that calculates the drag force on a particle

---
+++ applications/NSE_Particle/ICs_and_BCs.cc
@@ -0,0 +1,93 @@
+// ===========================================================================
+// FUNCTION FOR INITIAL CONDITIONS
+// ===========================================================================
+
+template <int dim, int degree>
+void customPDE<dim, degree>::setInitialCondition(const dealii::Point<dim>& p, const unsigned int index, double& scalar_IC, dealii::Vector<double>& vector_IC)
+{
+    // ---------------------------------------------------------------------
+    // ENTER THE INITIAL CONDITIONS HERE
+    // ---------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the initial condition for each variable
+    // according to its variable index
+
+    // The initial condition is a set of overlapping circles/spheres defined
+    // by a hyperbolic tangent function. The center of each circle/sphere is
+    // given by "center" and its radius is given by "radius".
+    double center[3] = { 5.0, 8.0, 0.0 };
+    double ellipseAxes[3] = { 1.0, 1.0, 1.0}; 
+    scalar_IC = 0;
+    double dist = 0.0;
+    double rad = 1.0;
+    for (unsigned int dir = 0; dir < dim; dir++) {
+        dist += (p[dir] - center[dir]) * (p[dir] - center[dir]) / ellipseAxes[dir] / ellipseAxes[dir];
+    }
+    dist = std::sqrt(dist);
+    double particle = 0.5 * (1.0 - std::tanh((dist - rad) / (W * std::sqrt(2))));
+
+    // Velocity
+    if (index == 0) {
+        for (unsigned int d = 0; d < dim; d++) {
+            vector_IC(d) = 0.0;
+            /*if (d == 0){
+                double height = 1.0;
+                double normalizedPos = p[1]/userInputs.domain_size[1];
+                vector_IC(d) = height*(1.0-4.0*(normalizedPos-0.5)*(normalizedPos-0.5));
+            }*/
+        }
+    }
+    // Pressure
+    else if (index == 1) {
+        scalar_IC = 0.0;
+    }
+    // Solid-liquid Order
+    else if (index == 2) {
+        scalar_IC = 2.0 * particle - 1.0;
+        //scalar_IC = - 1.0;
+    }
+    // Dimensionless Temperature
+    else {
+        scalar_IC = 0.0;
+    }
+
+    // ---------------------------------------------------------------------
+}
+
+// ===========================================================================
+// FUNCTION FOR NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS
+// ===========================================================================
+
+template <int dim, int degree>
+void customPDE<dim, degree>::setNonUniformDirichletBCs(const dealii::Point<dim>& p, const unsigned int index, const unsigned int direction, const double time, double& scalar_BC, dealii::Vector<double>& vector_BC)
+{
+    // --------------------------------------------------------------------------
+    // ENTER THE NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS HERE
+    // --------------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the boundary condition for each variable
+    // according to its variable index. This function can be left blank if there
+    // are no non-uniform Dirichlet boundary conditions. For BCs that change in
+    // time, you can access the current time through the variable "time". The
+    // boundary index can be accessed via the variable "direction", which starts
+    // at zero and uses the same order as the BC specification in parameters.in
+    // (i.e. left = 0, right = 1, bottom = 2, top = 3, front = 4, back = 5).
+
+    if(index == 0){
+        if(direction == 0){
+            double MaxFlow = 0.009;
+            double stepheight = 0.0;
+            double normalizedPos = p[1]/userInputs.domain_size[1];
+            //MaxFlow = MaxFlow*timeFactor;
+            if(p[1]/userInputs.domain_size[1] >= stepheight){
+                double b = std::abs(1.0/(0.5*stepheight-0.5));
+                vector_BC(0) = MaxFlow*(1.0-b*b*(normalizedPos-0.5-0.5*stepheight)*(normalizedPos-0.5-0.5*stepheight));
+            }
+            else{
+                vector_BC(0) = 0.0;
+            }
+        }
+    }
+
+    // -------------------------------------------------------------------------
+}
