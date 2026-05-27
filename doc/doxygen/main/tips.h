d0e9eb18b7d4b73d272f4846a3382d0b3874f56f
landinjm <landinjm@umich.edu>
2026-05-27 12:51:14 -0400

    temp

--- doc/doxygen/main/tips.h
+++ doc/doxygen/main/tips.h
@@ -1,5 +1,13 @@
 /** \page tips Tips & Tricks
 
-Don't use our software.
+- How to make code readable
+- How to make code performant
+  - Don't have extraneous dependencies
+- Tensor indexing (please don't)
+- Using built-in functions
+- Issues with too many subdivisions
+- How to start from scratch
+- How to debug code
+- How to make an issue
 
 */
